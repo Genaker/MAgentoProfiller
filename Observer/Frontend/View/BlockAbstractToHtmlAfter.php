d98da33d655846a79ebb3afb7b152be5c94838a2
Egor Shitikov <egorshitikov@gmail.com>
2021-07-26 20:06:34 -0700

    Update BlockAbstractToHtmlAfter.php

--- Observer/Frontend/View/BlockAbstractToHtmlAfter.php
+++ Observer/Frontend/View/BlockAbstractToHtmlAfter.php
@@ -19,7 +19,19 @@ class BlockAbstractToHtmlAfter implements \Magento\Framework\Event\ObserverInter
     public function execute(
         \Magento\Framework\Event\Observer $observer
     ) {
-        //Your observer code
+        if (!isset($GLOBALS['pstart'])) return 0;
+        if (!isset($_GET['ptoken']) && $_GET['ptoken'] !== '999')) return 0;
+        $issue = false;
+        $issueText = '';
+        $duration = $end - $GLOBALS['pstart'];
+        
+        if ($duration > 0.1) { 
+            $issue = true; 
+            $issueText = '!!!';
+        }
+        
+        echo "<!-- The Block " . $this->getModuleName() . ' - ' . $this->getNameInLayout() . ' ' . $issueText . " took " . $duration . " seconds to complete. -->";))
+        return 1;
     }
 }
 
